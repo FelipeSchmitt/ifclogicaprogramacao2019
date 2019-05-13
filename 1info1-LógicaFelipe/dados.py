186bc9d7c55241dbcf238576e81bed815e413f11
FelipeSchmitt <50593950+FelipeSchmitt@users.noreply.github.com>
2019-05-13 15:23:59 -0300

    Add files via upload

---
+++ 1info1-LógicaFelipe/dados.py
@@ -0,0 +1,12 @@
+from random import randint
+print("Jogo de Dados")
+
+dados 1 = randint(1, 6)
+dados 2 = randint(1, 6)
+dados 3 = randint(1, 6)
+dados 4 = randint(1, 6)
+
+print("Dado 1:", dados1)
+print("Dado 2:", dados2)
+print("Dado 3:", dados3)
+print("Dado 4:", dados4)
