284bdb760a6bd33dc0a8d176083f307bfa72453d
FelipeSchmitt <50593950+FelipeSchmitt@users.noreply.github.com>
2019-05-13 15:36:48 -0300

    Add files via upload

---
+++ 1info1-Lógica-Felipe/dados.py
@@ -0,0 +1,14 @@
+#Programa dos dados
+#Felipe de Abreu Schmitt
+from random import randint
+print("Jogo de Dados (v0.1")
+
+dados 1 = randint(1, 6)
+dados 2 = randint(1, 6)
+dados 3 = randint(1, 6)
+dados 4 = randint(1, 6)
+
+print("Dado 1:", dados1)
+print("Dado 2:", dados2)
+print("Dado 3:", dados3)
+print("Dado 4:", dados4)
